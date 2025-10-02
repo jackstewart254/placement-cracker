a0c0f102fd874406d19f1c5d35fceda204ee780e
Jack Stewart <jackmcleanstewart49@gmail.com>
2025-10-02 06:41:32 +0100

    changes: 1. solved the left/right pane scrolling error

--- app/placements/page.tsx
+++ app/placements/page.tsx
@@ -3,8 +3,12 @@ import { Suspense } from "react";
 
 export default async function ProtectedPage() {
   return (
-    <Suspense fallback={<div>Loading cover letters...</div>}>
-      <Cards />
-    </Suspense>
+    <div className="w-full h-[calc(100vh-4rem)] flex flex-col">
+      <div className="flex-1 overflow-hidden">
+        <Suspense fallback={<div>Loading cover letters...</div>}>
+          <Cards />
+        </Suspense>
+      </div>
+    </div>
   );
 }
